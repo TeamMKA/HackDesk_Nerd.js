678fc4316afd4c70e2fc60507a5fe9fc42bd0c95
Mustafa <yusufmustufa@gmail.com>
2024-09-27 16:59:20 +0530

    Added Styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
